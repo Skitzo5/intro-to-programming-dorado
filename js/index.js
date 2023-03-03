ef47a31934a8a3bfb506e0233898f59ac03fb1cc
Louie Mendoza <123456789+odin@users.noreply.github.com>
2023-03-02 21:47:41 -0500

    finish lesson 4.2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
-new Date(1 / 10 / 2023)
 
-const today = getFullYear
+/* Mentors Version
+let today = new Date();
+const today_year = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+const myName = "Louie";
+copyright.innerHTML = myName + " " + today_year;
+footer.appendChild(copyright);
+*/
+
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const myName = "Louie";
+console.log(thisYear);
 
 //this will select the "footer" in the document
 const footer = document.querySelector("footer");
@@ -9,4 +22,37 @@ const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
 
 //set the inner HTML to the copyright element previously created to display my name and year
-document.querySelector("footer").innerHTML = "©" + thisYear + "Louie Mendoza";
+//document.querySelector("footer").innerHTML = "©" + thisYear + "Louie Mendoza";
+
+//Using "DOM Manipulation", append the copyright element to the footer
+//document.appendChild(copyright);
+copyright.innerHTML = `Louie Mendoza &copy ${thisYear} &#9835`;
+footer.appendChild(copyright);
+
+const skills = [
+    "HTML",
+    "CSS",
+    "NODE.JS",
+    "Musician",
+];
+
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
+
+
+
+
+
+
+
+
+
+
+
+
